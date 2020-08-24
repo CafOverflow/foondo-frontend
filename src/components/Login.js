cdbdbe51f91a7165ec6fe408519396b4c1c422d6
Maria Kuzmicheva <kuzmichevamariia@gmail.com>
2020-08-24 11:59:22 +0200

    implemented fetch to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,27 +3,66 @@ import { Link } from 'react-router-dom';
 import { AppContext } from './Context';
 
 function Login() {
-  const [localState, setState] = useState({
+  const [emailState, setEmail] = useState({
     value: '',
-    // password: '',
   });
+  console.log(emailState);
+
+  const [passwordState, setPassword] = useState({
+    password: '',
+  });
+  console.log(passwordState);
 
   const {
-    handleNameChange,
+    handleEmailChange,
   } = useContext(AppContext);
 
   const handleValueChange = event => {
-    setState({ value: event.target.value });
+    setEmail({
+      value: event.target.value,
+    });
+  };
+
+  const handlePasswordChange = event => {
+    setPassword({
+      password: event.target.value,
+    });
+  };
+
+  const body = {
+    data: {
+      password: passwordState.password,
+      email: emailState.value,
+    },
+  };
+
+  const sendData = async () => {
+    await fetch('http://localhost:3001/login', {
+      mode: 'cors',
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    }).then(res => res.json())
+      .then(json => localStorage.setItem('jwt', json.jwt));
   };
 
   const handleSubmit = event => {
     event.preventDefault();
-    if (!localState.value) {
+    if (!emailState.value || !passwordState.password) {
       // eslint-disable-next-line no-alert
       window.alert('Please enter correct information!');
     } else {
-      handleNameChange(localState.value);
-      setState({ value: '' });
+      handleEmailChange(emailState.value);
+      sendData();
+      setEmail({
+        value: '',
+      });
+      setPassword({
+        password: '',
+      });
     }
   };
 
@@ -33,15 +72,15 @@ function Login() {
       <input
         className="login-form-input"
         type="text"
-        value={localState.value}
+        value={emailState.value}
         onChange={handleValueChange}
-        placeholder="Enter your username" />
-      {/* <input
+        placeholder="Enter your e-mail" />
+      <input
         className="login-form-input"
         type="text"
-        value={localState.password}
-        onChange={handleValueChange}
-        placeholder="Enter your password" /> */}
+        value={passwordState.password}
+        onChange={handlePasswordChange}
+        placeholder="Enter your password" />
       <input
         className="login-form-button"
         type="submit"
